7afdd6660f90623ce5cbf674c2f2eb36c9874e42
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2013-12-04 16:06:14 +0100

    merged latest version from dev

---
+++ src/udpkit.examples.simple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdpKit;
+
+namespace udpkit.examples.simple {
+    class Program {
+        static void Main (string[] args) {
+
+        }
+    }
+}
